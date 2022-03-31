396d9d54e3f9208f4da88879031b8f16f23b5295
Slocksan <977005@gmail.com>
2022-03-31 16:22:19 +0500

    changed directory

---
+++ Database/ExpeditionsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpeditionProject.Database.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpeditionProject.Database
+{
+    public class ExpeditionsContext : DbContext
+    {
+        public DbSet<Position> Positions { get; set; }
+
+        public ExpeditionsContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+        }
+    }
+}
---
+++ Database/Model/Position.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpeditionProject.Database.Model
+{
+    [Table("POSITIONS")]
+    public class Position
+    {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+    }
+}
